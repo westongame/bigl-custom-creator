0c7efe3cc4cd721c66a73bf7e5f947422700f59e
Mike Weston <mike@westongame.com>
2017-03-28 13:03:58 +0300

    Expand button functionality

--- src/components/MenuPreset/index.js
+++ src/components/MenuPreset/index.js
@@ -7,17 +7,73 @@ export default class MenuPreset extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            collapse: this.props.menuProps.links.length > 3,
+        };
+
+        this.showBtnClick = this.showBtnClick.bind(this);
+        this.hideBtnClick = this.hideBtnClick.bind(this);
         this.renderLinks = this.renderLinks.bind(this);
     }
 
+    showBtnClick() {
+        this.setState({
+            collapse: false,
+        });
+    }
+
+    hideBtnClick() {
+        this.setState({
+            collapse: true,
+        });
+    }
+
     renderLinks() {
-        return this.props.menuProps.links.map((item, index) =>
-            <div className={css.categoryTree__item} key={index}>
-                <a className={css.categoryTree__link} href={item.href ? item.href : '#'}>
-                    {item.text ? item.text : 'New link'}
-                </a>
-            </div>
-        );
+        const links = [];
+
+        this.props.menuProps.links.forEach((item, index) => {
+            links.push(
+                <div className={css.categoryTree__item} key={index}>
+                    <a className={css.categoryTree__link} href={item.href ? item.href : '#'}>
+                        {item.text ? item.text : 'New link'}
+                    </a>
+                </div>
+            );
+        });
+
+        if (this.props.isPreviewMode && this.state.collapse) {
+            const hiddenItemsCount = this.props.menuProps.links.length - 3
+
+            links.splice(3, hiddenItemsCount);
+
+            links.push(
+                <div className={css.categoryTree__item} key='showMore'>
+                    <a
+                        className={css.categoryTree__expandLink}
+                        onClick={this.showBtnClick}
+                    >
+                        {`Show ${hiddenItemsCount} more`}
+                    </a>
+                </div>
+            );
+
+            return links;
+        }
+
+        if (this.props.isPreviewMode && this.props.menuProps.links.length > 3 && !this.state.collapse) {
+            links.push(
+                <div className={css.categoryTree__item} key='hide'>
+                    <a
+                        className={css.categoryTree__expandLink}
+                        onClick={this.hideBtnClick}
+                    >
+                        {`Hide items`}
+                    </a>
+                </div>
+            );
+        }
+
+        return links;
     }
 
     render() {
